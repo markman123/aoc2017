c526dfa662b871de21a582f209db073aeb279037
Mark Marlow <themarlzy@gmail.com>
2017-12-03 10:41:41 +1100

    solution v1 - working but ugly

--- 01/captcha/README.md
+++ 01/captcha/README.md
@@ -1,6 +1,6 @@
-# Captcha
+# Captcha (Day 1)
 
-**TODO: Add description**
+The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
 
 ## Installation
 
--- 01/captcha/lib/captcha.ex
+++ 01/captcha/lib/captcha.ex
@@ -2,8 +2,8 @@ defmodule Captcha do
 
   defstruct(
     current_sum: 0,
+    rem_dig: [],
     first_dig: 0,
-    prev_dig: 0
   )
   @doc """
   The captcha requires you to review a sequence of digits
@@ -29,18 +29,42 @@ defmodule Captcha do
   """
   def captcha(digits) do
     digits
+    |> convert_to_list()
+    |> process_list(%__MODULE__{}, true)
+  end
+
+  def convert_to_list(digits) do
+    digits
     |> to_string()
     |> String.codepoints()
     |> Enum.map(fn x -> x |> String.to_integer end)
-    |> IO.inspect
   end
 
-  def process_list(list) do
-      
+  def process_list(list = [first_dig | _rest ], state, _is_first_dig = true) do
+    new_state = %{state | first_dig: first_dig}
+    process_list(list, new_state)
+  end
+
+  def process_list([first | []], state) do
+    new_sum = state.current_sum + process_dig(first, state.first_dig)
+    new_state = %{state | current_sum: new_sum,
+                  rem_dig: []
+                 }
+    process_list([], new_state)
+  end
+  def process_list([], state), do: state.current_sum
+  def process_list(list, state) do
+    [ first | rest ] = list
+    [ next | _ ] = rest
+
+    new_state = %{state | current_sum: state.current_sum + process_dig(first, next),
+              rem_dig: rest
+                 }
+    process_list(rest, new_state)
   end
 
   def process_dig(dig1, dig1) do
-    dig1 * 2
+    dig1
   end
   def process_dig(_, _) do
     0
